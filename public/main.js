51e75e1f7cc0f140ed3984b2e3758817ef4d9088
Naima Africa Francis <naima.africa@gmail.com>
2020-09-20 15:20:55 -0400

    Got the name change to work but stuck on the counter

--- public/main.js
+++ public/main.js
@@ -1,7 +1,54 @@
-function main() {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+// function main() {
+
+function handleChangeTeamOneName(event) {
+  const elementThatChanged = event.target
+  const inputFieldValue = elementThatChanged.value
+
+  const teamNameElement = document.querySelector('.team1 h2')
+
+  teamNameElement.textContent = inputFieldValue
 }
 
-document.addEventListener('DOMContentLoaded', main)
+const teamOneInput = document.querySelector('.team1 input')
+teamOneInput.addEventListener('input', handleChangeTeamOneName)
+
+//
+
+function handleChangeTeamTwoName(event) {
+  const elementThatChanged = event.target
+  const inputFieldValue = elementThatChanged.value
+
+  const teamNameElement = document.querySelector('.team2 h2')
+
+  teamNameElement.textContent = inputFieldValue
+}
+
+const teamTwoInput = document.querySelector('.team2 input')
+teamTwoInput.addEventListener('input', handleChangeTeamTwoName)
+
+//
+
+function clickCounter(event) {
+  let counter = 0
+
+  const counterClickedOn = event.target
+
+  if (counterClickedOn.classList.contains('add fas fa-2x fa-plus-circle')) {
+    counter++
+  }
+  if (
+    counterClickedOn.classList.contains('subtract fas fa-2x fa-minus-circle')
+  ) {
+    counter--
+  }
+
+  const score = document.querySelector('h3')
+  score.innerText = `${counter}`
+}
+const allScores = document.querySelectorAll('h3')
+
+allScores.forEach(element => element.addEventListener('click', clickCounter))
+
+// }
+
+// document.addEventListener('DOMContentLoaded', main)
